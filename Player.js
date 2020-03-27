b79f8955f1131cf6da1432f83e6d3fcb149370c7
Ivett Ördög <devillmeister@gmail.com>
2020-03-27 21:27:00 +0100

    Update Player.js

--- Player.js
+++ Player.js
@@ -1,10 +1,10 @@
 class Player {
   static get VERSION() {
-    return '0.1';
+    return '0.2';
   }
 
   static betRequest(gameState, bet) {
-    bet(0);
+    bet(10000);
   }
 
   static showdown(gameState) {
